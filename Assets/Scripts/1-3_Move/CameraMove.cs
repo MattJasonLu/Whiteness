59b880ac3bd1017d03a2ea1de15b4bc169227c99
MurderousCrotch <1132464757@qq.com>
2020-04-22 21:08:50 +0900

    [Matt]初步移动

---
+++ Assets/Scripts/1-3_Move/CameraMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+
+	public float speed = 5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float h = Input.GetAxisRaw("Horizontal");
+		float v = Input.GetAxisRaw("Vertical");
+
+		Vector3 targetCamPos = transform.position + new Vector3(-h, 0, 0) + new Vector3(0, 0, -v);
+		transform.position = Vector3.Lerp(transform.position, targetCamPos, speed * Time.deltaTime);
+	}
+}
