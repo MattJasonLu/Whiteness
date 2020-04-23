8a73b07a0fa739332bc7d984c974a40d5825808e
MurderousCrotch <1132464757@qq.com>
2020-04-23 21:38:10 +0900

    [Matt]战斗面板连接

---
+++ Assets/Scripts/1-4_Battle/BasicPanelControl.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicPanelControl : MonoBehaviour {
+
+	// 各类面板
+	public GameObject magicPanel;
+	public GameObject skillPanel;
+	public GameObject comboPanel;
+	public GameObject itemPanel;
+
+	void Awake()
+	{
+		// 启动时禁用面板
+		magicPanel.SetActive(false);
+		skillPanel.SetActive(false);
+		comboPanel.SetActive(false);
+		itemPanel.SetActive(false);
+	}
+
+	// 普通攻击
+	public void CommonAttack()
+	{
+
+	}
+
+	// 显示魔法面板
+	public void ShowMagicPanel()
+	{
+		magicPanel.SetActive(true);
+		gameObject.SetActive(false);
+	}
+
+	// 显示战技面板
+	public void ShowSkillPanel()
+	{
+		skillPanel.SetActive(true);
+		gameObject.SetActive(false);
+	}
+
+	// 显示连击面板
+	public void ShowComboPanel()
+	{
+		comboPanel.SetActive(true);
+		gameObject.SetActive(false);
+	}
+
+	// 显示物品面板
+	public void ShowItemPanel()
+	{
+		itemPanel.SetActive(true);
+		gameObject.SetActive(false);
+	}
+
+	// 撤退
+	public void Retreat()
+	{
+
+	}
+
+
+}
